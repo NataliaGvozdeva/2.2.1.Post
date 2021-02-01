5083bfd15cde2a06525be75a7c7c670452844c19
Natalia Gvozdeva <gvozdeva-natalia@mail.ru>
2021-02-01 18:00:22 +0300

    Added new test-case updateFunction_returnFalse

--- src/test/kotlin/ru/netology/lesson4/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/lesson4/WallServiceTest.kt
@@ -21,7 +21,7 @@ class WallServiceTest {
     }
 
     @Test
-    fun updateExisting() {
+    fun updateFunction_returnTrue() {
         // создаём целевой сервис
         val service = WallService()
         // заполняем несколькими постами
@@ -37,4 +37,23 @@ class WallServiceTest {
         // проверяем результат (используйте assertTrue или assertFalse)
         assertTrue(result)
     }
+
+    @Test
+    fun updateFunction_returnFalse() {
+        // создаём целевой сервис
+        val service = WallService()
+        // заполняем несколькими постами
+        service.add(Post(text = "First post"))
+        service.add(Post(text = "Second post"))
+        service.add(Post(text = "Third post"))
+        // создаём информацию об обновлении
+        val update = Post(id = 4, text = "updated text of the post")
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertFalse(result)
+    }
+
 }
